7d34e78ab845112c0ca836840e49bcc5e7a04836
易成杰 <626659321@qq.com>
2019-12-10 21:35:38 +0800

    init

---
+++ hello-world/src/main/java/com/yicj/hello/HelloBootstrap.java
@@ -0,0 +1,11 @@
+package com.yicj.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBootstrap.class,args) ;
+    }
+}
---
+++ hello-world/src/test/java/com/yicj/hello/HelloBootstrapTest.java
@@ -0,0 +1,14 @@
+package com.yicj.hello;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloBootstrapTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
