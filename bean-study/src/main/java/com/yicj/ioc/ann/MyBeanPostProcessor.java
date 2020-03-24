6fbb32fef34651355de306591ccc52094c4767be
易成杰 <626659321@qq.com>
2020-03-24 22:32:14 +0800

    bean实例化流程

---
+++ bean-study/src/main/java/com/yicj/ioc/ann/MyBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package com.yicj.ioc.ann;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        if(beanName.equals("worker")){
+            return new Worker() ;
+        }
+        return null ;
+    }
+
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        if(beanName.equals("worker")){
+            Worker worker = (Worker) bean ;
+            worker.setName("王五");
+        }
+        return false;
+    }
+}
---
+++ bean-study/src/main/java/com/yicj/ioc/ann/Worker.java
@@ -0,0 +1,11 @@
+package com.yicj.ioc.ann;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@ Data
+public class Worker {
+
+    private String name ;
+}
