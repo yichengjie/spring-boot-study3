92944c34dfea7ab146a6d21aab79e07537fbaad5
yicj <626659321@qq.com>
2020-03-27 18:29:45 +0800

    保存

--- starter-study/src/main/java/com/yicj/starter/condition/A.java
+++ starter-study/src/main/java/com/yicj/starter/condition/A.java
@@ -3,9 +3,10 @@ package com.yicj.starter.condition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+
 @Component
-//@MyConditionalAnnotation("com.mooc.condition")
-@MyConditionalAnnotation({"hello","world"})
+//@ConditionalOnProperty("com.mooc.condition")
+//@MyConditionalAnnotation({"hello","world"})
 public class A {
 
 }
