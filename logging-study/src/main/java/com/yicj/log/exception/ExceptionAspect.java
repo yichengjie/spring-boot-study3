7794cc165fc7475de7346faafab2c3c682b51a48
易成杰 <626659321@qq.com>
2020-03-29 10:48:03 +0800

    保存

--- logging-study/src/main/java/com/yicj/log/exception/ExceptionAspect.java
+++ logging-study/src/main/java/com/yicj/log/exception/ExceptionAspect.java
@@ -3,18 +3,20 @@ package com.yicj.log.exception;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 @Aspect
+@Component
 public class ExceptionAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(ExceptionAspect.class) ;
 
-    @AfterThrowing(pointcut = "within(com.yicj.log..*)", throwing = "ex")
-    public void handleException(JoinPoint joinPoint, Exception ex) throws Exception{
+    @AfterThrowing(pointcut = "within(com.yicj.log.controller.*)", throwing = "ex")
+    public void handleException(JoinPoint joinPoint, Throwable ex) throws Throwable{
         Class declaringType = joinPoint.getSignature().getDeclaringType();
         String clazz = declaringType.getCanonicalName();
         String name = joinPoint.getSignature().getName();
