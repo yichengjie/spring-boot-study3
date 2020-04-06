e9e1548a928bb7e0fcd2ac4b1cd13a792909d786
易成杰 <626659321@qq.com>
2020-04-06 14:02:44 +0800

    aop单独用一个项目

--- aop-study/transaction-study/src/main/java/com/yicj/dao/OperationLogDao.java
+++
@@ -1,7 +0,0 @@
-package com.yicj.dao;
-
-import com.yicj.domain.OperationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperationLogDao extends JpaRepository<OperationLog, Long> {
-}
--- aop-study/transaction-study/src/main/java/com/yicj/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.yicj.dao;
-
-import com.yicj.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-}
